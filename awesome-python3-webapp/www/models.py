a915c15debe2ed882e6bdffd9b898b705eb122e6
Zhangyongming <790454963@qq.com>
2018-12-20 10:53:06 +0800

    day3

--- awesome-python3-webapp/www/models.py
+++ awesome-python3-webapp/www/models.py
@@ -7,7 +7,42 @@ from orm import Model, StringField, BooleanField, FloatField, TextField
 
 
 def next_id():
-    # 自动生成ID
+    # 自动生成ID,id是由15d的时间和uuid的uuid4 32位16进制生成伪随机数
     return '%15d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
-class User(Model)
+
+class User(Model):
+    __table__ = 'user'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default='next_id', ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    create_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+    __table__ = 'comment'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image =StringField(ddl='varchar(500)')
+    content = TextField()
+    create_at = FloatField(default=time.time)
